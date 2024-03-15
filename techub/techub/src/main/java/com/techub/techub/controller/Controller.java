5f3722862e603ab0719d61b33f4aee0f36d16b72
Kamil Zeberga <kamil.insa@gmail.com>
2024-03-14 21:36:25 -0500

    Spring app demo with github actions

---
+++ techub/techub/src/main/java/com/techub/techub/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.techub.techub.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        StringBuilder sb = new StringBuilder();
+        return sb.append("Welcome to our first spring app").toString();
+    }
+}
